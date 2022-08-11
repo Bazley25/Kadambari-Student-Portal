39eb1b714d2564e2145acef05ef745470ff9fc8f
Bazly Anan <bazley421@gmail.com>
2022-08-12 00:07:44 +0600

    add header old file

---
+++ src/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/uikit.min.css">
+  <link rel="stylesheet" href="css/slick.css">
+  <link rel="stylesheet" href="css/slick-theme.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="css/responsive.dataTables.min.css">
+  <script src="https://kit.fontawesome.com/0b5a974696.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="scss/style.scss">
+  <link rel="shortcut icon" type="image/x-icon" href="favicon/khs.png">
+  <title>কদমবাড়ী উচ্চবিদ্যালয় প্রাক্তন শিক্ষার্থী সম্মিলন ২০২০</title>
+</head>
+<body>
+
+   <section id="showcase" class="bg-success">
+        <nav class="navbar navbar-dark  navbar-expand-md" uk-sticky="top: 5; animation: uk-animation-slide-top; bottom: #sticky-on-scroll-up">
+  <div class="container ">
+    <a href="index.php" class="navbar-brand ">কদমবাড়ী উচ্চবিদ্যালয় </a>
+    <button class="navbar-toggler navbar-toggler-right" data-toggle="collapse" data-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div id="navbarNav" class="collapse navbar-collapse ">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="insert.php">registration</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="support.php">notice</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">contact </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        sign up/log in
+        </a>
+        <div class="dropdown-menu bg-success text-light" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">sign up</a>
+          <a class="dropdown-item" href="#">log in</a>
+        </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+   </section>
+
+<!-- News section-->
+
+<section class="news bg-dark">
+        <div class="container">
+        <div class="row">
+            <div class="col-md">
+              <marquee class="text-light">*** কদমবাড়ী উচ্চবিদ্যালয় প্রাক্তন শিক্ষার্থী সম্মিলন ২০২০ এ আপনাকে স্বাগতম ***  আপনি  একবারই রেজিস্ট্রেশন করতে পারবেন। যদি কোন প্রকার ভুল করে থাকেন তবে আপনার তথ্য আপডেড করার জন্য ইমেইল করুন: shubhamandal70@gmail.com****
+            </marquee>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- News section end-->
